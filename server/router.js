6d9ebbad1a55e08e237bd90f6bbe04eb3b8357c6
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-04-13 09:58:55 +1000

    Cleaning...

--- server/router.js
+++ server/router.js
@@ -1,11 +1,14 @@
-var bibleApi = require('./bibleApi');
 var locallydb = require('locallydb');
 var db = new locallydb('db');
-var collection = db.collection('verses');
+var collection = db.collection('testcollection');
 
 var router = module.exports = require('express').Router();
 
 router
+  .get('/', function (req, res, next) {
+    res.writeHead(200);
+    res.end();
+  })
   .get('/healthcheck', function (req, res, next) {
     res.writeHead(200);
     res.end();
